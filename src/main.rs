8b076e9ebffaf2f351369087a2267b66c8c099be
Xavier Perez <duosrx@gmail.com>
2016-04-06 11:58:57 -0700

    Implement a few more instructions and clean the flag code

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
-use std::error::Error;
-use std::fs::File;
-use std::io::prelude::*;
-use std::path::Path;
+// use std::error::Error;
+// use std::fs::File;
+// use std::io::prelude::*;
+// use std::path::Path;
 
 // TODO: Implement Absolute and Indirect addressing modes
 // TODO: Implement a shitton more instructions
 // TODO: Factor out the memory code (we'll need to handle mappers and such)
+
+const CARRY_FLAG:    u8 = 0b00000001;
+const ZERO_FLAG:     u8 = 0b00000010;
+const IRA_FLAG:      u8 = 0b00000100;
+const DECIMAL_FLAG:  u8 = 0b00001000;
+const BREAK_FLAG:    u8 = 0b00010000;
+const OVERFLOW_FLAG: u8 = 0b01000000;
+const NEGATIVE_FLAG: u8 = 0b10000000;
 
 #[allow(dead_code)]
 #[derive(Debug)]
@@ -22,7 +30,7 @@ impl Registers {
     fn new() -> Registers {
         Registers {
             a: 0,
-            x: 2,
+            x: 0,
             y: 0,
             p: 0,
             s: 0,
@@ -31,6 +39,7 @@ impl Registers {
     }
 }
 
+// The addressing mode trait was liberally inspired by sprocketnes
 trait AddressingMode {
     fn load(&self, cpu: &mut Cpu) -> u8;
     fn store(&self, cpu: &mut Cpu, value: u8);
@@ -53,7 +62,7 @@ impl AddressingMode for MemoryAM {
 struct ImmediateAM;
 impl AddressingMode for ImmediateAM {
     fn load(&self, cpu: &mut Cpu) -> u8 { cpu.load_byte_and_inc_pc() }
-    fn store(&self, cpu: &mut Cpu, value: u8) { panic!("uhhh I can't store using Immediate Addressing Mode") }
+    fn store(&self, _cpu: &mut Cpu, _value: u8) { panic!("uhhh I can't store using Immediate Addressing Mode") }
 }
 
 struct Ram {
@@ -86,7 +95,46 @@ impl Cpu {
     fn new() -> Cpu {
         Cpu {
             regs: Registers::new(),
-            ram: Ram { val: [0; 0x800] }
+            ram: Ram {
+                val: [0; 0x800]
+            }
+        }
+    }
+
+    fn step(&mut self) {
+        let instruction = self.load_byte_and_inc_pc();
+        self.execute_instruction(instruction);
+        // TODO: Handle cycle count
+    }
+
+    fn execute_instruction(&mut self, instruction: u8) {
+        match instruction {
+            // 0x00 => self.brk(),
+            //0x01 => { self.indexed_indirect_x(); self.ora() }
+            // 0x05 => { self.zero_page(); self.ora() }
+            // 0x06 => { self.zero_page(); self.asl() }
+            // 0x08 => self.php(),
+            // 0x09 => { self.immediate(); self.ora() }
+            // 0x0D => { self.absolute(); self.ora() }
+
+            0x0A => self.asl(AccumulatorAM),
+            0x06 => { let am = self.zero_page(); self.asl(am) }
+            0x16 => { let am = self.zero_page_x(); self.asl(am) }
+            //0x0E => { let am = self.absolute(); self.asl(am) }
+            //0x1E => { let am = self.absolute_x(); self.asl(am) }
+
+            0xEA => (), // NOP
+            0xAA => self.tax(),
+            0xA8 => self.tay(),
+            0x8A => self.txa(),
+            0x9A => self.tya(),
+            0xE8 => self.inx(),
+            0xA9 => self.lda(ImmediateAM),
+            0xAD => { let am = self.zero_page(); self.lda(am) }
+            0xB5 => { let am = self.zero_page_x(); self.lda(am) }
+            0xA2 => self.ldx(ImmediateAM),
+            0xA0 => self.ldy(ImmediateAM),
+            _ => () //println!("Unknown opcode")
         }
     }
 
@@ -107,16 +155,24 @@ impl Cpu {
 
     fn set_nz_flags(&mut self, value: u8) {
         if value == 0 {
-            self.regs.p |= 0x02;
+            self.set_flag(ZERO_FLAG)
         } else {
-            self.regs.p &= 0xfd;
+            self.unset_flag(ZERO_FLAG)
         }
 
         if (value & 0x80) != 0 {
-            self.regs.p |= 0x80;
+            self.set_flag(NEGATIVE_FLAG)
         } else {
-            self.regs.p &= 0x7f;
+            self.unset_flag(NEGATIVE_FLAG)
         }
+    }
+
+    fn set_flag(&mut self, flag: u8) {
+        self.regs.p |= flag;
+    }
+
+    fn unset_flag(&mut self, flag: u8) {
+        self.regs.p &= !flag;
     }
 
     // Generate addressing modes
@@ -158,14 +214,7 @@ impl Cpu {
         self.set_nz_flags(val)
     }
 
-    // Index registers
-    fn inx(&mut self) {
-        let x = self.regs.x + 1;
-        self.regs.x = x;
-        self.set_nz_flags(x);
-    }
-
-    // Register Storage
+    // Accumulator - Storage
     fn tax(&mut self) {
         let a = self.regs.a;
         self.set_nz_flags(a);
@@ -189,61 +238,34 @@ impl Cpu {
         self.set_nz_flags(y);
         self.regs.a = y;
     }
+
+    // Acummulator - Bitwise logic
+    // Index registers X - Arithmetic
+    fn inx(&mut self) {
+        let x = self.regs.x + 1;
+        self.regs.x = x;
+        self.set_nz_flags(x);
+    }
+
+    fn asl<AM:AddressingMode>(&mut self, am: AM) {
+        let value = am.load(self);
+        if (value & 0x80) != 0 {
+            self.set_flag(CARRY_FLAG);
+        }
+        let result = value << 1;
+        self.set_nz_flags(result);
+        am.store(self, result)
+    }
 }
 
 fn main() {
-    let path = Path::new("/Users/xavier/code/rust/sen/missile1.h");
-    let display = path.display();
-
-    let mut file = match File::open(&path) {
-        Err(why) => panic!("couldn't open {}: {}", display, Error::description(&why)),
-        Ok(file) => file,
-    };
-
-    let mut buffer = Vec::new();
-
-    match file.read_to_end(&mut buffer) {
-        Err(why) => panic!("couldn't read {}: {}", display, Error::description(&why)),
-        Ok(_) => println!("{} is: {} bytes long", display, buffer.len()),
-    }
-
-    // let mut offset = 0;
-    // for chunk in buffer.chunks(16) {
-    //     print!("{:07x} ", offset);
-    //     for b in chunk {
-    //         print!("{:02x} ", b);
-    //     }
-    //     offset += 16;
-    //     println!("");
-    // }
-    // println!("{:07x} ", offset);
-
-    let mut i = 0;
-
     let mut cpu = Cpu::new();
-
-    while i < buffer.len() {
-        match buffer[i] {
-            0xEA => (), // NOP
-            0xAA => cpu.tax(),
-            0xA8 => cpu.tay(),
-            0x8A => cpu.txa(),
-            0x9A => cpu.tya(),
-            0xE8 => cpu.inx(),
-            0xA9 => cpu.lda(ImmediateAM),
-            0xAD => { let am = cpu.zero_page(); cpu.lda(am) }
-            0xB5 => { let am = cpu.zero_page_x(); cpu.lda(am) }
-            0xA2 => cpu.ldx(ImmediateAM),
-            0xA0 => cpu.ldy(ImmediateAM),
-            _ => () //println!("Unknown opcode")
-        }
-
-        i += 1;
-    }
-
-    cpu.ram.val[0] = 123;
-    cpu.lda(MemoryAM { address: 0 });
+    cpu.ram.val[0] = 0x0A;
+    cpu.regs.a = 129;
+    // cpu.ram.val[0] = 0xA9;
+    // cpu.ram.val[1] = 185;
+    cpu.step();
 
     println!("{:?}", cpu);
-    //println!("{:08b}", cpu.regs.p);
+    println!("Flags: {:08b}", cpu.regs.p);
 }
