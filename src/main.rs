afb0fe2cffb35f7f0eba066301bad3d29fce0266
Xavier Perez <duosrx@gmail.com>
2016-04-06 15:26:05 -0700

    Implement flag and stack instructions

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,13 @@
 // TODO: Implement a shitton more instructions
 // TODO: Factor out the memory code (we'll need to handle mappers and such)
 
-const CARRY_FLAG:    u8 = 0b00000001;
-const ZERO_FLAG:     u8 = 0b00000010;
-const IRA_FLAG:      u8 = 0b00000100;
-const DECIMAL_FLAG:  u8 = 0b00001000;
-const BREAK_FLAG:    u8 = 0b00010000;
-const OVERFLOW_FLAG: u8 = 0b01000000;
-const NEGATIVE_FLAG: u8 = 0b10000000;
+const CARRY_FLAG:     u8 = 0b00000001;
+const ZERO_FLAG:      u8 = 0b00000010;
+const INTERRUPT_FLAG: u8 = 0b00000100;
+const DECIMAL_FLAG:   u8 = 0b00001000;
+//const BREAK_FLAG:    u8 = 0b00010000;
+const OVERFLOW_FLAG:  u8 = 0b01000000;
+const NEGATIVE_FLAG:  u8 = 0b10000000;
 
 #[allow(dead_code)]
 #[derive(Debug)]
@@ -32,8 +32,8 @@ impl Registers {
             a: 0,
             x: 0,
             y: 0,
-            p: 0,
-            s: 0,
+            p: 0x24, // 0b00100100
+            s: 0xFD, // 253
             pc: 0,
         }
     }
@@ -109,32 +109,56 @@ impl Cpu {
 
     fn execute_instruction(&mut self, instruction: u8) {
         match instruction {
-            // 0x00 => self.brk(),
-            //0x01 => { self.indexed_indirect_x(); self.ora() }
-            // 0x05 => { self.zero_page(); self.ora() }
-            // 0x06 => { self.zero_page(); self.asl() }
-            // 0x08 => self.php(),
-            // 0x09 => { self.immediate(); self.ora() }
-            // 0x0D => { self.absolute(); self.ora() }
+            // 0x00 => self.brk(), TODO
+            0xEA => (), // NOP
+
+            // Flag operations
+            0x18 => self.clc(),
+            0x38 => self.sec(),
+            0x58 => self.cli(),
+            0x78 => self.sei(),
+            0xB8 => self.clv(),
+            0xD8 => self.cld(),
+            0xF8 => self.sed(),
+
+            // 0x01 => { let am = self.indexed_indirect_x(); self.ora(am) }
+            // 0x11 => { let am = self.indexed_indirect_y(); self.ora(am) }
+            0x09 => self.ora(ImmediateAM),
+            0x0D => { let am = self.absolute(); self.ora(am) }
+            0x1D => { let am = self.absolute_x(); self.ora(am) }
+            0x19 => { let am = self.absolute_y(); self.ora(am) }
+            0x05 => { let am = self.zero_page(); self.ora(am) }
+            0x15 => { let am = self.zero_page_x(); self.ora(am) }
+
+            // Stack
+            0x08 => self.php(),
+            0x28 => self.plp(),
+            0x48 => self.pha(),
+            0x68 => self.pla(),
+
+            // Register
+            0xAA => self.tax(),
+            0x8A => self.txa(),
+            0xCA => self.dex(),
+            0xE8 => self.inx(),
+            0xA8 => self.tay(),
+            0x98 => self.tya(),
+            0x88 => self.dey(),
+            0xC8 => self.iny(),
 
             0x0A => self.asl(AccumulatorAM),
             0x06 => { let am = self.zero_page(); self.asl(am) }
             0x16 => { let am = self.zero_page_x(); self.asl(am) }
-            //0x0E => { let am = self.absolute(); self.asl(am) }
-            //0x1E => { let am = self.absolute_x(); self.asl(am) }
+            0x0E => { let am = self.absolute(); self.asl(am) }
+            0x1E => { let am = self.absolute_x(); self.asl(am) }
 
-            0xEA => (), // NOP
-            0xAA => self.tax(),
-            0xA8 => self.tay(),
-            0x8A => self.txa(),
-            0x9A => self.tya(),
-            0xE8 => self.inx(),
             0xA9 => self.lda(ImmediateAM),
             0xAD => { let am = self.zero_page(); self.lda(am) }
             0xB5 => { let am = self.zero_page_x(); self.lda(am) }
             0xA2 => self.ldx(ImmediateAM),
             0xA0 => self.ldy(ImmediateAM),
-            _ => () //println!("Unknown opcode")
+
+            unknown => panic!("Unkown opcode {:02x}", unknown)
         }
     }
 
@@ -148,8 +172,27 @@ impl Cpu {
 
     fn load_byte_and_inc_pc(&mut self) -> u8 {
         let pc = self.regs.pc;
-        let byte = self.ram.load(pc);
+        let byte = self.load_byte(pc);
         self.regs.pc += 1;
+        byte
+    }
+
+    fn load_word_and_inc_pc(&mut self) -> u16 {
+        let a = self.load_byte_and_inc_pc() as u16;
+        let b = self.load_byte_and_inc_pc() as u16;
+        a | (b << 8)
+    }
+
+    fn push_byte(&mut self, value: u8) {
+        let stack_pointer = self.regs.s;
+        self.store_byte(0x100 + stack_pointer as u16, value);
+        self.regs.s -= 1;
+    }
+
+    fn pop_byte(&mut self) -> u8 {
+        let stack_pointer = self.regs.s;
+        let byte = self.load_byte(0x100 + stack_pointer as u16);
+        self.regs.s += 1;
         byte
     }
 
@@ -186,15 +229,38 @@ impl Cpu {
         MemoryAM { address: address as u16 }
     }
 
-    fn zero_page_y(&mut self) -> MemoryAM {
-        let address = self.load_byte_and_inc_pc() + self.regs.y;
-        MemoryAM { address: address as u16 }
+    //fn zero_page_y(&mut self) -> MemoryAM {
+    //    let address = self.load_byte_and_inc_pc() + self.regs.y;
+    //    MemoryAM { address: address as u16 }
+    //}
+
+    fn absolute(&mut self) -> MemoryAM {
+        let address = self.load_word_and_inc_pc();
+        MemoryAM { address: address }
     }
 
-    fn sta<AM: AddressingMode>(&mut self, am: AM) {
+    fn absolute_x(&mut self) -> MemoryAM {
+        let address = self.load_word_and_inc_pc() + self.regs.x as u16;
+        MemoryAM { address: address }
+    }
+
+    fn absolute_y(&mut self) -> MemoryAM {
+        let address = self.load_word_and_inc_pc() + self.regs.y as u16;
+        MemoryAM { address: address }
+    }
+
+    // FIXME
+    /*fn indexed_indirect(&mut self) -> MemoryAM {
+        let byte = self.load_byte_and_inc_pc();
+        let indirect = byte + self.regs.x;
+        let address = self.load_word()
+        MemoryAM { address: address }
+    }*/
+
+    /*fn sta<AM: AddressingMode>(&mut self, am: AM) {
         let a = self.regs.a;
         am.store(self, a)
-    }
+    }*/
 
     fn lda<AM: AddressingMode>(&mut self, am: AM) {
         let val = am.load(self);
@@ -214,17 +280,11 @@ impl Cpu {
         self.set_nz_flags(val)
     }
 
-    // Accumulator - Storage
+    // Register
     fn tax(&mut self) {
         let a = self.regs.a;
         self.set_nz_flags(a);
         self.regs.x = a;
-    }
-
-    fn tay(&mut self) {
-        let a = self.regs.a;
-        self.set_nz_flags(a);
-        self.regs.y = a;
     }
 
     fn txa(&mut self) {
@@ -233,18 +293,40 @@ impl Cpu {
         self.regs.a = x;
     }
 
+    fn dex(&mut self) {
+        let x = self.regs.x - 1;
+        self.regs.x = x;
+        self.set_nz_flags(x);
+    }
+
+    fn inx(&mut self) {
+        let x = self.regs.x + 1;
+        self.regs.x = x;
+        self.set_nz_flags(x);
+    }
+
+    fn tay(&mut self) {
+        let a = self.regs.a;
+        self.set_nz_flags(a);
+        self.regs.y = a;
+    }
+
     fn tya(&mut self) {
         let y = self.regs.y;
         self.set_nz_flags(y);
         self.regs.a = y;
     }
 
-    // Acummulator - Bitwise logic
-    // Index registers X - Arithmetic
-    fn inx(&mut self) {
-        let x = self.regs.x + 1;
-        self.regs.x = x;
-        self.set_nz_flags(x);
+    fn dey(&mut self) {
+        let y = self.regs.y - 1;
+        self.regs.y = y;
+        self.set_nz_flags(y);
+    }
+
+    fn iny(&mut self) {
+        let y = self.regs.y + 1;
+        self.regs.y = y;
+        self.set_nz_flags(y);
     }
 
     fn asl<AM:AddressingMode>(&mut self, am: AM) {
@@ -256,16 +338,74 @@ impl Cpu {
         self.set_nz_flags(result);
         am.store(self, result)
     }
+
+    fn ora<AM:AddressingMode>(&mut self, am: AM) {
+        let result = self.regs.a | am.load(self);
+        self.set_nz_flags(result);
+        self.regs.a = result;
+    }
+
+    // Stack operations
+    fn pha(&mut self) {
+        let a = self.regs.a;
+        self.push_byte(a)
+    }
+
+    fn php(&mut self) {
+        let a = self.regs.a;
+        self.push_byte(a)
+    }
+
+    fn plp(&mut self) {
+        self.regs.p = self.pop_byte()
+    }
+
+    fn pla(&mut self) {
+        self.regs.a = self.pop_byte()
+    }
+
+    // Flags operations
+    fn clc(&mut self) {
+        self.unset_flag(CARRY_FLAG);
+    }
+
+    fn sec(&mut self) {
+        self.set_flag(CARRY_FLAG);
+    }
+
+    fn cli(&mut self) {
+        self.unset_flag(INTERRUPT_FLAG);
+    }
+
+    fn sei(&mut self) {
+        self.set_flag(INTERRUPT_FLAG);
+    }
+
+    fn clv(&mut self) {
+        self.unset_flag(OVERFLOW_FLAG);
+    }
+
+    fn cld(&mut self) {
+        self.unset_flag(DECIMAL_FLAG);
+    }
+
+    fn sed(&mut self) {
+        self.set_flag(DECIMAL_FLAG);
+    }
 }
 
 fn main() {
     let mut cpu = Cpu::new();
-    cpu.ram.val[0] = 0x0A;
-    cpu.regs.a = 129;
-    // cpu.ram.val[0] = 0xA9;
-    // cpu.ram.val[1] = 185;
-    cpu.step();
+    println!("{:?}", cpu);
+    println!("Flags: {:08b}", cpu.regs.p);
+    cpu.ram.val[0] = 0x08;
+    cpu.ram.val[1] = 0x68;
 
+    cpu.step();
+    println!("{:?}", cpu);
+    println!("Flags: {:08b}", cpu.regs.p);
+
+    cpu.step();
     println!("{:?}", cpu);
     println!("Flags: {:08b}", cpu.regs.p);
 }
