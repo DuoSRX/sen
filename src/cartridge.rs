132f102b4f0b266cb1320abfba300c4a76b9a4d6
Xavier Perez <duosrx@gmail.com>
2016-04-08 09:52:01 -0700

    Start Cartridge loading

---
+++ src/cartridge.rs
@@ -0,0 +1,62 @@
+//use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+
+// Reference: http://wiki.nesdev.com/w/index.php/INES
+
+#[allow(dead_code)]
+struct NesHeader {
+    magic: [u8; 4], // Magic number ("NES\x1a")
+    prg_size: u8,   // PRG Rom banks (by increments of 16KB)
+    chr_size: u8,   // CHR Rom banks (by increments of 8KB)
+    flags_6: u8,
+    flags_7: u8,
+    ram_size: u8,   // PRG Ram size (by increments of 8KB)
+    //unused: [u8; 7] // Unused stuff
+}
+
+#[allow(dead_code)]
+pub struct Cartridge {
+    prg: Vec<u8>,
+    chr: Vec<u8>,
+    ram: [u8; 0x2000]
+}
+
+// TODO: Handle EOF errors
+fn file_to_buffer(mut buffer: &mut [u8], file: &mut File) {
+    let mut i = 0;
+    while i < buffer.len() {
+        let n = file.read(&mut buffer[i..]).unwrap();
+        i += n
+    }
+}
+
+impl Cartridge {
+    pub fn load(mut file: &mut File) -> Cartridge {
+        let mut header: [u8; 16] = [0; 16];
+        file_to_buffer(&mut header, &mut file);
+
+        let header = NesHeader {
+            magic: [header[0], header[1], header[2], header[3]],
+            prg_size: header[4],
+            chr_size: header[5],
+            flags_6: header[6],
+            flags_7: header[7],
+            ram_size: header[8]
+        };
+
+        let prg_len = header.prg_size as usize * 0x4000;
+        let mut prg_rom = vec![0; prg_len];
+        file_to_buffer(&mut prg_rom, &mut file);
+
+        let chr_len = header.chr_size as usize * 0x2000;
+        let mut chr_rom = vec![0; chr_len];
+        file_to_buffer(&mut chr_rom, &mut file);
+
+        Cartridge {
+            prg: prg_rom,
+            chr: chr_rom,
+            ram: [0; 0x2000]
+        }
+    }
+}
