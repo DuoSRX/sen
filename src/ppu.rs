c93f7022383bda569c025c2c3792f2a0f8e06692
Xavier Perez <duosrx@gmail.com>
2016-04-20 12:20:22 -0700

    Some palettes work

--- src/ppu.rs
+++ src/ppu.rs
@@ -10,14 +10,30 @@ mod ppuctrl {
 
 #[allow(dead_code)]
 mod ppumask {
-    pub const BACKGROUND:      u8 = 0b00001000;
-    pub const SPRITES:         u8 = 0b00010000;
+    pub const BACKGROUND: u8 = 0x08;
+    pub const SPRITES:    u8 = 0x10;
 }
 
 pub struct PpuResult {
     pub new_frame: bool,
     pub nmi: bool
 }
+
+const PALETTE_RGB: [u32; 64] = [
+    0x666666, 0x002A88, 0x1412A7, 0x3B00A4, 0x5C007E,
+    0x6E0040, 0x6C0600, 0x561D00, 0x333500, 0x0B4800,
+    0x005200, 0x004F08, 0x00404D, 0x000000, 0x000000,
+    0x000000, 0xADADAD, 0x155FD9, 0x4240FF, 0x7527FE,
+    0xA01ACC, 0xB71E7B, 0xB53120, 0x994E00, 0x6B6D00,
+    0x388700, 0x0C9300, 0x008F32, 0x007C8D, 0x000000,
+    0x000000, 0x000000, 0xFFFEFF, 0x64B0FF, 0x9290FF,
+    0xC676FF, 0xF36AFF, 0xFE6ECC, 0xFE8170, 0xEA9E22,
+    0xBCBE00, 0x88D800, 0x5CE430, 0x45E082, 0x48CDDE,
+    0x4F4F4F, 0x000000, 0x000000, 0xFFFEFF, 0xC0DFFF,
+    0xD3D2FF, 0xE8C8FF, 0xFBC2FF, 0xFEC4EA, 0xFECCC5,
+    0xF7D8A5, 0xE4E594, 0xCFEF96, 0xBDF4AB, 0xB3F3CC,
+    0xB5EBF2, 0xB8B8B8, 0x000000, 0x000000,
+];
 
 // struct Sprite {
 //     pub x: u8,
@@ -159,7 +175,6 @@ impl Ppu {
         if address < 0x2000 {
             self.cartridge.chr[address as usize]
         } else if address < 0x3F00 {
-            //self.name_tables[address as usize & 0x0FFF]
             self.name_tables[address as usize & 0x07FF]
         } else if address < 0x4000 {
             self.palettes[address as usize & 0x1F]
@@ -170,7 +185,7 @@ impl Ppu {
 
     pub fn vram_store(&mut self, address: u16, value: u8) {
         if address < 0x2000 {
-            //self.cartridge.chr[address as usize] = value;
+            // Can you even write to CHR...?
         } else if address < 0x3F00 {
             self.name_tables[address as usize & 0x07FF] = value;
         } else if address < 0x4000 {
@@ -281,16 +296,16 @@ impl Ppu {
                 let mut offset2 = (current_tile << 4) as u16 + (scanline as u16) % 8;
                 offset2 += if self.regs.control & ppuctrl::BG_PATTERN_TABLE_ADDRESS == 0 { 0 } else { 0x1000 };
 
-
                 let p0 = self.vram_load(offset2);
                 let p1 = self.vram_load(offset2 + 8);
                 let bit0 = (p0 >> (7 - ((x % 8) as u8))) & 1;
                 let bit1 = (p1 >> (7 - ((x % 8) as u8))) & 1;
                 let result = (bit1 << 1) | bit0;
 
-                // FIXME: handle palettes and RGB
-                let c = (result << 6) as u32;
-                self.set_pixel(x as u32, scanline as u32, (c << 8) | (c << 16) | (c << 24));
+                let palette_address = 0x3F00 + result as u16 & 0x3F;
+                //let palette = self.vram_load(palette_address);
+                let c = PALETTE_RGB[palette_address as usize];
+                self.set_pixel(x as u32, scanline as u32, c);
             } else {
                 self.set_pixel(x as u32, scanline as u32, 0);
             }
